0d6b3513de7b2b5d26a3749b779611296a8492c3
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-18 00:18:09 -0600

    State and Rendering in Class-Based Component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,12 +3,34 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    render () {
+    constructor(props){
+        super(props);
+
+        this.state = {lat: null, errorMessage: null};
+
         window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
-        return <div>Latitude: </div>;
+    }
+
+    render () { 
+        if(!this.state.lat && !this.state.errorMessage){
+            return <div> Loading! </div>;
+        }
+        else if(this.state.lat){
+            return <div> Latitude: {this.state.lat} </div>
+        }
+        else if(this.state.errorMessage){
+            return <div> Error: {this.state.errorMessage} </div>
+        }
+        
+        // return (
+        // <div>
+        //     Latitude: {this.state.lat}<br/>
+        //     Error: {this.state.errorMessage}
+        // </div>
+        // );
     }
 }
 
