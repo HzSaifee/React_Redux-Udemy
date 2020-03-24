e4d205d3d82231512d5a20c877a1b19452872446
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-24 14:43:22 -0600

    Completed  My Exercise: Youtube Videos Library

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 const KEY = 'AIzaSyDE5WEMohKWGlWUsoUd9j_EPtQGiGme2k0';
 
 class App extends React.Component {
-    state = {videos: []};
+    state = {videos: [], selectedVideo: null };
+
+    componentDidMount() {
+        this.onTermSubmit('Youtube Rewind');
+    }
 
     onTermSubmit = async term => {
         const response = await youtube.get("/search", {
@@ -17,15 +23,27 @@ class App extends React.Component {
                 key: KEY
             }
         });
-
-        this.setState({ videos: response.data.items});
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0]});
     }
+
+    onVideoSelect = video => {
+        this.setState({ selectedVideo: video });
+      };
 
     render(){
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                {this.state.videos.length}
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={this.state.selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                    </div>
+                </div>
+            </div>
             </div>
         );
     }
