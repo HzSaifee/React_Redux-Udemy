c2088e0567cc31e0d7d8849ba3a01e304ee1ccf8
Huzefa Saifee <hzsaifee52@gmail.com>
2020-04-21 17:25:25 -0600

    Learning about Reducers Rules and working

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,7 +4,13 @@ export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts'); 
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
-}
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
 
 // export const fetchPosts = () => {
 //     return async function(dispatch, getState) {
--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -5,5 +5,4 @@ export default (state=[], action) => {
         default:
             return state;
     }
-
 }
